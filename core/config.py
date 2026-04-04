bda57113ed6184cadd03c11d320eff989848d88c
Praveen Gamini <praveengamini009@gmail.com>
2026-04-05 01:19:02 +0530

    .env fix

--- core/config.py
+++ core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     SUPABASE_URL: str
     SUPABASE_KEY: str
     DATABASE_URL: str
+    ADMIN_EMAIL: str = "admin@example.com"
+    ADMIN_PASSWORD: str = "Admin1234!"
 
     model_config = {
         "env_file": ".env"
